3448caa152f85d192b5f2db062b4eb55e2e8b2f9
Eduardo Estrada <ceed.educacion@gmail.com>
2019-04-15 16:08:19 -0300

    Actualizaccion Bakend

--- models/usuario.js
+++ models/usuario.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
-
-
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
+
+var rolesValidos ={
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no permitido'
+}
 
 var usuarioSchema = new Schema({
 
@@ -9,7 +13,9 @@ var usuarioSchema = new Schema({
     email: { type: String, unique:true, required: [true, 'El correo es necesario'] },
     password: { type: String, required: [true, 'La contraseña es necesario'] },
     img: { type: String, required: false },
-    role: { type: String, required: true, default:'USER_ROLE'}
+    role: { type: String, required: true, default:'USER_ROLE', enum: rolesValidos}
 });
+
+    usuarioSchema.plugin( uniqueValidator, { message: '{PATH} debe ser único'});
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
